7850891bf0de681cb092f842a09cd780220d4c9e
yang <yang>
2019-07-15 18:24:17 +0800

    111

---
+++ src/effectivejava/MeiLaoBan.java
@@ -0,0 +1,19 @@
+package effectivejava;
+
+public class MeiLaoBan {
+    int totalM;
+    int lNum;
+    int anum;
+    int bnum;
+    int a;
+    int b;
+    public void runTo (int num){
+        totalM-=num;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/effectivejava/chapter2/item6/Sum.java
@@ -0,0 +1,32 @@
+package effectivejava.chapter2.item6;
+
+import javax.sound.midi.Soundbank;
+import java.util.Comparator;
+
+// Hideously slow program! Can you spot the object creation? (Page 24)
+public class Sum {
+    private static long sum() {
+        Long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++)
+            sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("!1111");
+        System.out.println("333");
+        int numSets = Integer.parseInt(args[0]);
+        long x = 0;
+
+        for (int i = 0; i < numSets; i++) {
+            long start = System.nanoTime();
+            x += sum();
+            long end = System.nanoTime();
+            System.out.println((end - start) / 1_000_000. + " ms.");
+        }
+
+        // Prevents VM from optimizing away everything.
+        if (x == 42)
+            System.out.println();
+    }
+}
